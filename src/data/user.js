9cbe4132db6c5611a625ea99e3ba66856fa852f0
tayathorn <tayathorn@thinknet.co.th>
2017-12-06 13:41:38 +0700

    create user data file

---
+++ src/data/user.js
@@ -0,0 +1,80 @@
+// ref: https://randomuser.me/api
+
+const User = [
+  {
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "valdemar",
+      last: "jensen"
+    },
+    location: {
+      street: "4892 svenstrupvej",
+      city: "københavn n",
+      state: "syddanmark",
+      postcode: 73176
+    },
+    email: "valdemar.jensen@example.com",
+    login: {
+      username: "crazyduck901",
+      password: "tranny",
+      salt: "kpV55xCk",
+      md5: "022da1b88c31723a02c8dd7739151bff",
+      sha1: "87eea7b3cda55cdd93b2c6292a79401a7bfc59b1",
+      sha256: "a5b4996f0afda6e9fa8af85e5ab45298fbc7f41a7fbb8050516a72de82d790dd"
+    },
+    dob: "1991-04-16 09:08:10",
+    registered: "2013-06-21 17:11:22",
+    phone: "11323086",
+    cell: "97512070",
+    id: {
+      name: "CPR",
+      value: "165897-9257"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/45.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/45.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/45.jpg"
+    },
+    nat: "DK"
+  },
+  {
+    gender: "female",
+    name: {
+      title: "mrs",
+      first: "lily",
+      last: "duncan"
+    },
+    location: {
+      street: "6353 west street",
+      city: "edinburgh",
+      state: "west glamorgan",
+      postcode: "R6A 2PQ"
+    },
+    email: "lily.duncan@example.com",
+    login: {
+      username: "silverrabbit442",
+      password: "qwertz",
+      salt: "hz3K6l9N",
+      md5: "97042dc89e879b7726252be8cb6bb299",
+      sha1: "383979a93fdb8f97b09475d27f300e76f97922e2",
+      sha256: "2fe23d310417da2c7af36a70183044eacaac0934617e01c870d8fc7ce1a70e58"
+    },
+    dob: "1949-03-20 16:43:05",
+    registered: "2010-09-12 02:45:58",
+    phone: "019467 65097",
+    cell: "0750-153-939",
+    id: {
+      name: "NINO",
+      value: "LS 87 79 14 V"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/45.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/45.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/45.jpg"
+    },
+    nat: "GB"
+  }
+]
+
+export default User
