c3c363344b72bd0cbb21c9c3bcf6c0540cab5b9e
tayathorn <tayathorn@thinknet.co.th>
2017-12-08 16:04:53 +0700

    add networkMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import { createNetworkMiddleware } from 'react-native-offline'
 
 import rootSaga from '../sagas'
 import reducers from '../reducers'
 
 const sagaMiddleware = createSagaMiddleware()
+const networkMiddleware = createNetworkMiddleware()
 
 const store = createStore(
   reducers,
-  applyMiddleware(sagaMiddleware)
+  applyMiddleware(networkMiddleware, sagaMiddleware)
 )
 
 sagaMiddleware.run(rootSaga)
