3edecc4b1d22440cd73f34a7602fae16c898b135
tayathorn <tayathorn@thinknet.co.th>
2017-12-08 13:25:35 +0700

    add Network Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux'
+import { reducer as network } from 'react-native-offline'
 
 import user from './user'
 
 export default combineReducers({
   user,
+  network,
 })
